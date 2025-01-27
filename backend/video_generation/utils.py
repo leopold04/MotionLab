fe0f3497aea513bb118730821b8da6fdddbbfc7e
Leopold Dorilas <lmd256@cornell.edu>
2025-01-27 03:30:46 -0500

    Dynamic pause/play gif video template and utility functions. Will change name later

---
+++ backend/video_generation/utils.py
@@ -0,0 +1,88 @@
+import ffmpeg
+import os
+from pydub import AudioSegment
+import requests
+from PIL import Image  # Importing PIL for image processing
+
+
+def remove_directory(path):
+    if os.path.exists(path):
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        # Remove the now-empty output directory
+        os.rmdir(path)
+
+
+def get_fps(bytes) -> int:
+    path = "file"
+    with open(path, "wb") as f:
+        f.write(bytes)
+    # Run ffmpeg to retrieve the metadata of the video
+    probe = ffmpeg.probe(path, v='error', select_streams='v:0',
+                         show_entries='stream=r_frame_rate')
+
+    # Extract the frame rate from the probe result
+    fps = probe['streams'][0]['r_frame_rate']
+    num, denom = map(int, fps.split('/'))
+    os.remove(path)
+    return int(num / denom)
+
+
+def compress_png(input_file, output_file, quality=80):
+    """
+    Compress a PNG image using Pillow.
+    - Convert the image to a palette-based format with 256 colors.
+    - Optimize the image to reduce the file size.
+    """
+    img = Image.open(input_file)
+    img = img.convert("P", colors=256)  # Convert to 256-color palette
+    img.save(output_file, optimize=True)  # Save optimized PNG
+
+
+def compress_images(output_folder):
+    """
+    Compress all PNG files in the 'output_folder' directory using the compress_png function.
+    """
+    for filename in os.listdir(output_folder):
+        if filename.endswith(".png"):
+            try:
+                input_path = os.path.join(output_folder, filename)
+                output_path = os.path.join(output_folder, f"frame{filename}")
+                compress_png(input_path, output_path)
+                # Optionally remove the original uncompressed image
+                os.remove(input_path)
+            except Exception as e:
+                print(e)
+
+
+# returns frame path, frame count, and fps
+def extract_frames(bytes, userID):
+    # later, make sure we are creating files in a unique place
+
+    path = "file"
+    with open(path, "wb") as f:
+        f.write(bytes)
+    output_folder = f"extracted{userID}"
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Use FFmpeg to extract frames as PNG images (still images, not video)
+    ffmpeg.input(path).output(
+        f'{output_folder}/%04d.png',
+        # Scale the frames to width 720 (height auto-calculated)
+        vf="scale=720:-1", loglevel="quiet"
+    ).run()
+
+    # Compress PNGs using PIL after extraction
+    compress_images(output_folder)
+    # Remove the video file from the filesystem after we are done
+    os.remove(path)
+    # be sure to return the fps of the
+    print("Frames extracted and compressed successfully!")
+    # number of frames we extracted
+    frame_count = len(os.listdir(output_folder))
+    # fps of our gif
+    frames_per_second = get_fps(bytes)
+
+    return {"output_folder": output_folder, "frame_count": frame_count, "fps": frames_per_second}
---
+++ frontend/src/graphics/animations/multisound.ts
@@ -0,0 +1,259 @@
+import emitter from "../utils/emitter.js";
+import AnimationConfig from "../utils/animation-config.js";
+import Particle from "../elements/particle.js";
+import Ring from "../elements/ring.js";
+import { createCanvas, Canvas, CanvasRenderingContext2D, loadImage } from "canvas";
+
+class BounceParticle {
+  canvas: HTMLCanvasElement | Canvas;
+  ctx: CanvasRenderingContext2D | any;
+  ring: Ring;
+  p1: Particle;
+  particles: Particle[];
+  frame: number = 0;
+  fps: number = 60;
+  duration: number;
+  audioTimeline: object[] = [];
+  config: AnimationConfig;
+  playing: boolean = false;
+  requestID: number | null = null; // Variable to hold the requestAnimationFrame ID
+  seed: number;
+  last: number = 0;
+  soundStart: number = 0;
+  sound: any;
+  sequenceURL: string;
+  sequenceFrameCount: number;
+  sequenceFPS: number;
+  songURL: string;
+  images: any = []; // frames of our video to be rendered onto the canvas
+  didx: number = 0; // essentially whether or not we progress frames / play the video (delta idx)
+  imgIdx: number = 0; // index of the image we are loading in
+  centerX: number;
+  centerY: number;
+  constructor(config: AnimationConfig) {
+    // generating headless with node js
+    if (typeof window === "undefined") {
+      this.canvas = createCanvas(config["canvas_width"], config["canvas_height"]);
+      this.ctx = this.canvas.getContext("2d")!;
+    } else {
+      this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
+      this.ctx = this.canvas.getContext("2d")!;
+      this.canvas.width = config["canvas_width"];
+      this.canvas.height = config["canvas_height"];
+    }
+    // loading in animation configurations
+    this.duration = config["duration"] * this.fps;
+    this.seed = config["seed"] as number;
+    this.songURL = config["collision_sound"] as string;
+    this.sequenceURL = config["sequence"] as string;
+    this.sequenceFPS = config["sequence_fps"] as number;
+    this.sequenceFrameCount = config["sequence_frame_count"] as number;
+
+    // scaling the animation appropriately (default is 720p)
+    let scaleFactor;
+    switch (config["canvas_width"]) {
+      case 480:
+        scaleFactor = 2 / 3;
+        break;
+      case 720:
+        scaleFactor = 1;
+        break;
+      case 1080:
+        scaleFactor = 3 / 2;
+        break;
+      default:
+        scaleFactor = 1;
+        break;
+    }
+    this.centerX = this.canvas.width / 2;
+    this.centerY = this.canvas.height / 2;
+    // position settings
+    const ringRadius = 0.8 * 360 * scaleFactor;
+    const p1Radius = 60 * scaleFactor;
+    const [p1vx, p1vy] = [4, 6].map((x) => scaleFactor * x);
+
+    const gravity = 0.25 * scaleFactor;
+
+    this.ring = new Ring(ringRadius, this.canvas, this.ctx);
+    // prettier-ignore
+    this.p1 = new Particle(this.centerX, this.centerY, p1Radius, p1vx, p1vy, gravity, this.ring, config["particle_1_color"]!,this.canvas, this.ctx);
+    // prettier-ignore
+
+    this.particles = [this.p1];
+    this.ring.randomizeParticlePositions(this.particles, this.seed);
+
+    // clearing event emitter
+    emitter.clear();
+    // initializing the emitter to listen for 'collision'
+    emitter.on("collision", () => this.handleBounce());
+    this.config = config;
+  }
+
+  // updating positions of all things (squares, containers, etc)
+  update() {
+    for (let p of this.particles) {
+      p.move();
+    }
+    for (let i = 0; i < this.particles.length; i++) {
+      for (let j = i + 1; j < this.particles.length; j++) {
+        Particle.handleCollision(this.particles[i], this.particles[j]);
+      }
+    }
+    this.frame++;
+  }
+
+  // loading images (TODO: add loading all audio)
+  // TODO: create failsafe if assets are not loaded in yet
+  async load() {
+    // sequenceURL = "supabase_url.com/bucket_path/"
+    for (let i = 1; i < this.sequenceFrameCount; i++) {
+      let imgURL = this.sequenceURL + `frame${i.toString().padStart(4, "0")}.png`;
+      let img = await loadImage(imgURL);
+      this.images.push(img);
+    }
+
+    // lmao fix this syntax
+    if (typeof window === "undefined") {
+    } else {
+      this.sound = new Audio(this.songURL);
+    }
+  }
+
+  // drawing all things
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Clear the canvas
+    // background
+    this.ctx.fillStyle = this.config["background_color"] as string;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    for (let p of this.particles) {
+      p.draw();
+    }
+    this.ring.draw();
+    this.renderImage();
+  }
+
+  /*
+  TODO: algorithmically determine how long each sound should be for headless generation
+  dont add to audio timeline until the end
+  instead collect times of bounces, then make audio timeline based off of those
+
+  */
+
+  // renders a single frame of the video to the screen
+  renderImage() {
+    // either choose to stay on the current image or go to the next depending on didx
+    this.imgIdx += this.didx;
+    let index = Math.round(this.imgIdx) % (this.sequenceFrameCount - 1);
+    // always end up having an image drawn
+    let currentImage = this.images[index];
+    let imgW = 0.4 * this.canvas.width;
+    try {
+      let imgH = imgW * (currentImage.height / currentImage.width); // keep aspect ratio
+      let imgX = this.centerX - imgW / 2;
+      let imgY = this.centerY - imgH / 2;
+      this.ctx.drawImage(currentImage, imgX, imgY, imgW, imgH);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  resizeParticle() {
+    this.p1.radius += 1;
+    // move along line to center so the ball does not stick to the walls
+    let delta = 1 / 100;
+    this.p1.pos.x += delta * (this.centerX - this.p1.pos.x);
+    this.p1.pos.y += delta * (this.centerY - this.p1.pos.y);
+  }
+  handleBounce() {
+    // 1 means the video is playing, 0 means it is not
+    console.log(this.frame);
+    let buffer = 60; // if bounce does not occur in buffer/60 seconds, pause and restart
+    this.resizeParticle();
+    // undefined
+    if (this.frame - this.last > buffer && this.last != 0) {
+      if (typeof window === "undefined") {
+        // cut the sound off and save it
+        let audio_duration = this.frame - this.soundStart;
+        // we want to convert this from frames to milliseconds
+        audio_duration = Math.round((audio_duration / 60) * 1000);
+        // TODO: implement some check so that if the end frame (duration * 60) is coming, we play?
+        // idk its not working for the last bounce, but we made good progress
+        this.audioTimeline.push({ audio: this.songURL, frame: this.soundStart, audio_duration: audio_duration });
+        // if last bounce is buffer from duration then play sound
+      } else {
+        // browser
+        this.sound.pause();
+        this.sound.currentTime = 0;
+      }
+      this.didx = 0; // pause the video
+    } else {
+      // start the audio
+      if (this.frame - this.soundStart > buffer || this.last == 0) {
+        if (typeof window === "undefined") {
+          this.soundStart = this.frame;
+          // if we are close to the end, play the rest of the audio
+          let audio_duration = this.duration - this.frame;
+          audio_duration = Math.round((audio_duration / 60) * 1000);
+          if (this.duration - this.frame < buffer) {
+            this.audioTimeline.push({ audio: this.songURL, frame: this.soundStart, audio_duration: audio_duration });
+          }
+        } else {
+          this.sound.play();
+        }
+        // ex: if gif is 60fps, then didx = 1, so every frame, we advance 1 gif frame
+        // if gif is 20fps, then didx = 20/60=1/3, so every 3 frames, we advance 1 gif frame
+        this.didx = this.sequenceFPS / this.fps;
+        console.log(this.didx);
+      }
+    }
+    this.last = this.frame;
+  }
+
+  // The animation function to animate
+  animate() {
+    if (this.playing) {
+      let lastTime = performance.now();
+      let frameTime = 1000 / this.fps; // Lock target framerate at fps
+
+      const loop = () => {
+        if (!this.playing) return; // Exit the loop if animation is paused
+
+        let now = performance.now();
+        let deltaTime = now - lastTime;
+
+        if (deltaTime >= frameTime) {
+          this.update();
+          this.draw();
+          lastTime = now - (deltaTime % frameTime); // Maintain consistent frame timing
+        }
+
+        this.requestID = requestAnimationFrame(loop); // Continue the loop
+      };
+
+      loop(); // Start the animation loop
+    }
+  }
+
+  // Pause the animation
+  pause() {
+    this.playing = false;
+    this.sound.pause();
+    if (this.requestID !== null) {
+      cancelAnimationFrame(this.requestID); // Cancel the animation frame request
+      this.requestID = null;
+    }
+  }
+
+  // Start playing the animation
+  play() {
+    if (this.sound.paused && this.frame > 1) {
+      this.sound.play();
+    }
+    if (!this.playing) {
+      this.playing = true;
+      this.animate(); // Start the animation loop if not already playing
+    }
+  }
+}
+
+export default BounceParticle;
